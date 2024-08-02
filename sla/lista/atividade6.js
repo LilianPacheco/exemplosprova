158c3d6a6348f932a4dc083690dd65d04b36cc89
LilianPacheco <162714070+LilianPacheco@users.noreply.github.com>
2024-08-02 12:17:00 -0300

    Add files via upload

---
+++ sla/lista/atividade6.js
@@ -0,0 +1,11 @@
+function calculaArea(largura, altura) {
+    return largura * altura;
+}
+
+console.log(calculaArea(5, 10));
+
+function saudacao(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+
+saudacao('SeuNome');
